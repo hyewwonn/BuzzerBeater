ba1c9becc3a3b244db17526382811607279c7fbc
hyewon <w2106@e-mirim.hs.kr>
2023-04-12 00:28:59 +0900

    공 회전 애니메이션 추가

--- buzzer-beater/src/App.js
+++ buzzer-beater/src/App.js
@@ -1,5 +1,14 @@
 import bg from './img/bg1.png';
 import logo from './img/logo.png';
+import goalpost from './img/goalpost.png';
+import bb from './img/blueball.png';
+import pb from './img/pinkball.png';
+import ob from './img/orangeball.png';
+import gb from './img/greenball.png';
+import be from './img/b-effect.png';
+import pe from './img/p-effect.png';
+import oe from './img/o-effect.png';
+import ge from './img/g-effect.png';
 import './App.css';
 
 function App() {
@@ -7,10 +16,14 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="bg-container">
-          <img className="background" src={bg}></img>
+          <img className="goalpost" src={goalpost} />
+          <img className="blueball" src={bb} />
+          <img className="orangeball" src={ob} />
+          <img className="pinkball" src={pb} />
+          <img className="greenball" src={gb} />
         </div>
         <div className="logo">
-          <img className="logo-img" src={logo}></img>
+          <img className="logo-img" src={logo}/>
         </div>
         <div className="btn">
           <div className="how-btn">게임방법</div>
