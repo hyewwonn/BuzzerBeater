7756e08999f47e181e87c7a6a18f3eeadeeb4af8
leehyeryeong <w2108@e-mirim.hs.kr>
2023-05-22 09:34:33 +0900

    score 글자끼리 간격, 미디어 쿼리 시작

--- buzzer-beater/src/index.js
+++ buzzer-beater/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import GameRule from './pages/GameRule';
 // import PCrule from './pages/PCrule';
 import reportWebVitals from './reportWebVitals';
+import GamePage from './pages/GamePage';
 // import GamePage from './pages/GamePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GameRule/>
+    <GamePage/>
   </React.StrictMode>
 );
 
