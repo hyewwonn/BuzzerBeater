a48f421c32dc2e4fed3d4c76ea0ede6882601b0c
hyewon <w2106@e-mirim.hs.kr>
2023-06-07 19:45:32 +0900

    gamepage css

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -17,19 +17,23 @@ export default function GamePage() {
     <div className={styles.box}>
       <div className={styles.top}>
         <div style={leftstyle} className={styles.score}>000</div>
-        <div style={rightstyle} className={styles.time_clock}>TIME 00</div>
+        <div style={rightstyle} className={styles.timeClock}>TIME 00</div>
       </div>
-      <div className={styles.balls}>
-        <Image className={styles.blueBall} src={bb} alt="blueball"/>
-        <Image className={styles.orangeBall} src={ob} alt="orangeball"/>
-        <Image className={styles.pinkBall} src={pb} alt="pinkball"/>
-        <Image className={styles.greenBall} src={gb} alt="greenball"/>
+      <div className={styles.ballsContainer}>
+        <div className={styles.balls}>
+          <Image className={styles.blueBall} src={bb} alt="blueball"/>
+          <Image className={styles.orangeBall} src={ob} alt="orangeball"/>
+          <Image className={styles.pinkBall} src={pb} alt="pinkball"/>
+          <Image className={styles.greenBall} src={gb} alt="greenball"/>
+        </div>
       </div>
-      <div className={styles.goal_posts}>
-        <Image className={styles.blue_goalpost} src={bgp} alt="blue-goalpost"/>
-        <Image className={styles.pink_goalpost} src={pgp} alt="pink-goalpost"/>
-        <Image className={styles.green_goalpost} src={ggp} alt="green-goalpost"/>
-        <Image className={styles.orange_goalpost} src={ogp} alt="orange-goalpost"/>
+      <div className={styles.goalPostsContainer}>
+        <div className={styles.goalPosts}>
+          <Image className={styles.blueGoalpost} src={bgp} alt="blue-goalpost"/>
+          <Image className={styles.pinkGoalpost} src={pgp} alt="pink-goalpost"/>
+          <Image className={styles.greenGoalpost} src={ggp} alt="green-goalpost"/>
+          <Image className={styles.orangeGoalpost} src={ogp} alt="orange-goalpost"/>
+        </div>
       </div>
     </div>
   );
