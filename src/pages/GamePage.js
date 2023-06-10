fe3ae1d79c26280c7469f99f542215be8580a59c
alszero <alszero05@gmail.com>
2023-06-10 13:14:00 +0900

    공이 랜덤으로 내려옴(여러개가 안나옴.. 수정필요)

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,9 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/GamePage.module.css';
 import Image from 'next/image';
 import bb from '../../public/img/blueball.png';
-import pb from '../../public/img/pinkball2.png';
+import pb from '../../public/img/pinkball.png';
 import ob from '../../public/img/orangeball.png';
-import gb from '../../public/img/greenball2.png';
+import gb from '../../public/img/greenball.png';
 import bgp from '../../public/img/bluegoalpost.png';
 import pgp from '../../public/img/pinkgoalpost.png';
 import ogp from '../../public/img/orangegoalpost.png';
@@ -12,6 +13,18 @@ import ggp from '../../public/img/greengoalpost.png';
 export default function GamePage() {
   const leftstyle = { float: 'left' };
   const rightstyle = { float: 'right' };
+  const ballImages = [bb, pb, ob, gb];
+  const [ballPosition, setBallPosition] = useState(0);
+  const [ballImage, setBallImage] = useState(ballImages[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBallPosition(prevPosition => prevPosition + 2);
+      setBallImage(ballImages[Math.floor(Math.random() * ballImages.length)]);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.box}>
@@ -20,19 +33,16 @@ export default function GamePage() {
         <div style={rightstyle} className={styles.timeClock}>TIME 00</div>
       </div>
       <div className={styles.ballsContainer}>
-        <div className={styles.balls}>
-          <Image className={styles.blueBall} src={bb} alt="blueball"/>
-          <Image className={styles.orangeBall} src={ob} alt="orangeball"/>
-          <Image className={styles.pinkBall} src={pb} alt="pinkball"/>
-          <Image className={styles.greenBall} src={gb} alt="greenball"/>
+        <div className={styles.balls} style={{ marginTop: `${ballPosition}px` }}>
+          <Image className={styles.ball} src={ballImage} alt="ball" width={100} height={100} />
         </div>
       </div>
       <div className={styles.goalPostsContainer}>
         <div className={styles.goalPosts}>
-          <Image className={styles.blueGoalpost} src={bgp} alt="blue-goalpost"/>
-          <Image className={styles.pinkGoalpost} src={pgp} alt="pink-goalpost"/>
-          <Image className={styles.greenGoalpost} src={ggp} alt="green-goalpost"/>
-          <Image className={styles.orangeGoalpost} src={ogp} alt="orange-goalpost"/>
+          <Image className={styles.blueGoalpost} src={bgp} alt="blue-goalpost" />
+          <Image className={styles.pinkGoalpost} src={pgp} alt="pink-goalpost" />
+          <Image className={styles.greenGoalpost} src={ggp} alt="green-goalpost" />
+          <Image className={styles.orangeGoalpost} src={ogp} alt="orange-goalpost" />
         </div>
       </div>
     </div>
